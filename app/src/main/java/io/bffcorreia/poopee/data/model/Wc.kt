8c802a2b5304d9c15d6d5867aa6a60abfee4ea67
Bruno Correia <bffcorreia@gmail.com>
2017-06-03 14:02:34 +0100

    Navigate to wc details.

--- app/src/main/java/io/bffcorreia/poopee/data/model/Wc.kt
+++ app/src/main/java/io/bffcorreia/poopee/data/model/Wc.kt
@@ -1,7 +1,9 @@
 package io.bffcorreia.poopee.data.model
 
+import java.io.Serializable
 import java.util.*
 
 data class Wc(var createdAt: Date, var updatedAt: Date, var code: String?,
     var location: String, var price: Float, var photoUrl: String?, var avgRate: Float,
-    var avgClean: Float, var avgPrivacy: Float, var avgStyle: Float, var reviews: List<Review>?)
+    var avgClean: Float, var avgPrivacy: Float, var avgStyle: Float,
+    var reviews: ArrayList<Review>) : Serializable
--- app/src/main/java/io/bffcorreia/poopee/presentation/Navigator.kt
+++ app/src/main/java/io/bffcorreia/poopee/presentation/Navigator.kt
@@ -3,6 +3,8 @@ package io.bffcorreia.poopee.presentation
 import android.app.Activity
 import android.content.Intent
 import io.bffcorreia.poopee.common.di.PerActivity
+import io.bffcorreia.poopee.data.model.Wc
+import io.bffcorreia.poopee.presentation.rate.RateActivity
 import io.bffcorreia.poopee.presentation.wc.WcActivity
 import io.bffcorreia.poopee.presentation.wcs.WcsActivity
 import javax.inject.Inject
@@ -16,7 +18,14 @@ import javax.inject.Inject
   }
 
   fun navigateToRate() {
+    val intent = Intent(activity, RateActivity::class.java)
+    activity.startActivity(intent)
+    activity.finish()
+  }
+
+  fun navigateToWc(wc: Wc) {
     val intent = Intent(activity, WcActivity::class.java)
+    intent.putExtra("wc", wc)
     activity.startActivity(intent)
     activity.finish()
   }
